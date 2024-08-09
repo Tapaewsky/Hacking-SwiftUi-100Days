51e7ca2b8de0afb81c7900a9fb8bdddd4ef17e09
Said Tapaev <120788741+Tapaewsky@users.noreply.github.com>
2024-08-09 16:51:37 +0300

    Initial Commit

---
+++ Hacking SwiftUi 100Days/Hacking_SwiftUi_100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacking_SwiftUi_100DaysApp.swift
+//  Hacking SwiftUi 100Days
+//
+//  Created by Said Tapaev on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hacking_SwiftUi_100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
